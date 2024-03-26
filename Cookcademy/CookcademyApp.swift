1cb65467a0b91a72faa0f13318fdf1a681d94bd3
Gustavo Vazquez <160083465+Great-Visions-Code@users.noreply.github.com>
2024-03-25 21:40:21 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Gustavo Vazquez on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

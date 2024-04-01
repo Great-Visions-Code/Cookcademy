0fc3d9ef4698b2bce7ae6a4e59dda1a3530fc5e6
Gustavo Vazquez <160083465+Great-Visions-Code@users.noreply.github.com>
2024-03-31 17:45:12 -0700

    Update RecipeDetailView.swift

--- Cookcademy/Views/RecipeDetailView.swift
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     let recipe: Recipe
-    
+
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
-    
+
     var body: some View {
         VStack {
             HStack {
@@ -23,40 +23,38 @@ struct RecipeDetailView: View {
             }
             HStack {
                 Text(recipe.mainInformation.description)
-                    .font(.subheadline)
+                    .font(.body)
                     .padding()
                 Spacer()
             }
             List {
                 Section(header: Text("Ingredients")) {
-                    ForEach(recipe.ingredients.indices, id: \.self) 
-                    { index in
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
                         let ingredient = recipe.ingredients[index]
                         Text(ingredient.description)
-                            .foregroundStyle(listTextColor)
+                            .foregroundColor(listTextColor)
                     }
-                } .listRowBackground(listBackgroundColor)
+                }.listRowBackground(listBackgroundColor)
                 Section(header: Text("Directions")) {
-                  ForEach(recipe.directions.indices, id: \.self) { index in
-                    let direction = recipe.directions[index]
-                    HStack {
-                      Text("\(index + 1). ").bold()
-                      Text("\(direction.isOptional ? "(Optional) " : "")"
-                            + "\(direction.description)")
-                    } .foregroundStyle(listTextColor)
-                  }
-                } .listRowBackground(listBackgroundColor)
-              }
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let direction = recipe.directions[index]
+                        HStack {
+                            Text("\(index + 1). ").bold()
+                            Text("\(direction.isOptional ? "(Optional) " : "")\(direction.description)")
+                        }.foregroundColor(listTextColor)
+                    }
+                }.listRowBackground(listBackgroundColor)
             }
+        }
         .navigationTitle(recipe.mainInformation.name)
     }
 }
 
 struct RecipeDetailView_Previews: PreviewProvider {
-  @State static var recipe = Recipe.testRecipes[0]
-  static var previews: some View {
-    NavigationView {
-      RecipeDetailView(recipe: recipe)
+    @State static var recipe = Recipe.testRecipes[0]
+    static var previews: some View {
+        NavigationView {
+            RecipeDetailView(recipe: recipe)
+        }
     }
-  }
 }
